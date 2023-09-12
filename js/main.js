0429c7aacb1ed40476bbab253f3fc8f68220c861
Yl1a <volkova.ylia2004@gmail.com>
2023-09-12 14:28:05 +0300

    t5_1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let square = '<div class="square"></div>';
+/* let square = '<div class="square"></div>'; */
 let SquareContainer = document.querySelector('.container_square');
 let number = document.querySelector('.text');
 
 let num = prompt ('Введите число');
 let y = 0;
 for (let x = 0; x < num; x++){
-    SquareContainer.insertAdjacentHTML('beforeend', square);
+    SquareContainer.insertAdjacentHTML('beforeend', `<div class="square">${x}</div>`);
 }
